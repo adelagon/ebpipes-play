d01394eac6b513dc166e807151a06531efd84f26
Alvin Delagon <me@adelagon.com>
2023-04-17 03:20:01 +0000

    provision queue

--- ebpipes_play/ebpipes_play_stack.py
+++ ebpipes_play/ebpipes_play_stack.py
@@ -10,10 +10,8 @@ class EbpipesPlayStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "EbpipesPlayQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # SQS Queue
+        queue = sqs.Queue(
+            self, "EbpipesPlayQueue",
+            visibility_timeout=Duration.seconds(300),
+        )
